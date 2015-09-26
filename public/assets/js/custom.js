6e24bdf548262e3be40b8cb084548ecd1a1d0c35
ayokesh <ayokesh.cse@gmail.com>
2015-09-26 19:44:07 +0530

    monster color animation are done

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,4 +1,19 @@
 function color(monster){
 	var color_val = $('#color_'+monster).val();
 	$('#color_span_'+monster).fadeIn(400).text('Color Code : '+color_val);
+
+	if(monster == 1){
+		$('#monster-inc').css('background-color',color_val);
+		$('.top').css('border-top-color',color_val);
+		$('head').append('<style>.m:after,.m:before{border-top-color:'+ color_val+' !important;}</style>');
+		$('head').append('<style>.bottom:after,.bottom:before{border-bottom-color:'+ color_val+' !important;}</style>');
+	}else if(monster == 2){
+		$('.m').css('background-color', color_val);
+	}else if(monster == 3){
+		$('.eye .up').css('background-color', color_val);
+		$('.eye .down').css('background-color', color_val);
+	}else if(monster == 4){
+		$('.eye .up .iris').css('background-color', color_val);
+		$('.eye .down .iris').css('background-color', color_val);
+	}
 }
