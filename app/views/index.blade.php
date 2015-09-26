897248874bd260933bc5008044bcb192eee3ee3d
ayokesh <ayokesh.cse@gmail.com>
2015-09-26 22:05:39 +0530

    added bootstrap model

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -22,10 +22,12 @@
 			<div class="canvas-item">
 				<div class="canvas">
 				<h5>{{{$color->likes}}} Likes <span class="time-ago">{{date('d M,Y',strtotime($color->created_at))}}</span></h5>
+						<a data-toggle="modal" data-target="#myModal">
 						<div class="canvas-1" style="background-color:#{{{$color->color_1}}};"></div>
 						<div class="canvas-2" style="background-color:#{{{$color->color_2}}};"></div>
 						<div class="canvas-3" style="background-color:#{{{$color->color_3}}};"></div>
 						<div class="canvas-4" style="background-color:#{{{$color->color_4}}};"></div>
+					</a>
 				</div>
 			</div>
 		</div>
@@ -34,4 +36,24 @@
 
 
 	</div>
+
+
+	<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 @stop
