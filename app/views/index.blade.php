673c7a7cb6d39dfa56b97409d0e52eb096fb22ca
ayokesh <ayokesh.cse@gmail.com>
2015-10-30 09:09:39 +0530

    color changes

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -22,7 +22,7 @@
 			<div class="canvas-item">
 				<div class="canvas">
 				<h5>{{{$color->likes}}} Likes <span class="time-ago">{{date('d M,Y',strtotime($color->created_at))}}</span></h5>
-						<a data-toggle="modal" data-target="#myModal">
+						<a data-toggle="modal" data-target="#model_{{$i}}">
 						<div class="canvas-1" style="background-color:#{{{$color->color_1}}};"></div>
 						<div class="canvas-2" style="background-color:#{{{$color->color_2}}};"></div>
 						<div class="canvas-3" style="background-color:#{{{$color->color_3}}};"></div>
@@ -31,6 +31,24 @@
 				</div>
 			</div>
 		</div>
+
+					<!-- Modal -->
+		<div class="modal fade" id="model_{{$i}}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 class="modal-title" id="myModalLabel">Modal title {{$i}}</h4>
+		      </div>
+		      <div class="modal-body">
+		        ...
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
 		<?php $i--; ?>
 		@endforeach
 
@@ -38,22 +56,6 @@
 	</div>
 
 
-	<!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+
 
 @stop
