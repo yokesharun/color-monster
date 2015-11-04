a84de8e867fb44cc664d1ff52099eb0c23d9377a
ayokesh <ayokesh.cse@gmail.com>
2015-11-04 09:45:57 +0530

    admin table added

---
+++ app/database/migrations/2015_11_03_165050_create_admin_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateAdminTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('admin', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email');
+			$table->string('password');
+			$table->string('first_name');
+			$table->string('last_name');
+			$table->string('profile_pic');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('admin');
+	}
+
+}
---
+++ app/models/Admin.php
@@ -0,0 +1,6 @@
+<?php
+
+class Admin extends \Eloquent {
+	protected $fillable = [];
+	protected $table = 'admin';
+}
